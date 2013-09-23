4cb6aba282f430ec8afbcec4ad6b93828bba0b31
Johan Carleson <johan@carleson.se>
2013-09-23 19:57:50 +0000

    Added a simple class

--- ssn.py
+++ ssn.py
@@ -1,19 +1,37 @@
-print ('==============================')
-print ('Social Security Number Validator')
-print ('')
-print ('version 0.1')
-print ('==============================')
-country = input('Enter Country (sv,dk, no, fi): ')
-ssn = input('Enter Social Security Number (SSN): ')
-ssn = str.replace(ssn,'-','')
-ssn = str.replace(ssn,'.','')
-ssn = ssn.strip()
+"""Social Security Number Validator, version 0.11 """
 
-if country == str.lower('sv'):
-        print ('Sweden')
-elif country == str.lower('no'):
-        print ('Norway')
-else:
-        print ('Unknown option !')
-print (ssn)
+class ssn:
+"""A simple class for validating Social Security Numbers"""
+
+def __init__(self, country, number):
+	self._number = number.strip()
+	self._number = str.replace(self._number, '-','')
+	self._number = str.replace(self._number, '.','')
+
+	self._country = str.lower(country)
+
+        if not (self._number.isdigit():
+			raise ValueError("Invalid Social Security Number '{}'".format(self._number))
+
+def IsValid(self):
+"""Check if selected Social Security Number is valid """
+	return true
+
+def Value(self):
+"""Get Social Security Number"""
+	return self._number
+
+# country = input('Enter Country (sv,dk, no, fi): ')
+# ssn = input('Enter Social Security Number (SSN): ')
+# ssn = str.replace(ssn,'-','')
+# ssn = str.replace(ssn,'.','')
+# ssn = ssn.strip()
+#
+# if country == str.lower('sv'):
+#        print ('Sweden')
+# elif country == str.lower('no'):
+#        print ('Norway')
+#else:
+#        print ('Unknown option !')
+#print (ssn)
 
