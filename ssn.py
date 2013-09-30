fd2478a432dfd2c814b49d528b60fe06211f8b03
Johan Carleson <johan.carleson@gmail.com>
2013-09-30 20:36:56 +0200

    Minor changes + Setting up PyCharm

--- ssn.py
+++ ssn.py
@@ -1,38 +1,63 @@
 """Social Security Number Validator, version 0.11 """
 
-class ssn:
+class SSN:
     """A simple class for validating Social Security Numbers"""
 
-def __init__(self, country, number):
-    self._number = number.strip()
-    self._number = str.replace(self._number, '-','')
-    self._number = str.replace(self._number, '.','')
+    def __init__(self, country, number):
+        self._number = number.strip()
+        self._number = str.replace(self._number, '-','')
+        self._number = str.replace(self._number, '.','')
 
-    self._country = str.lower(country)
+        self._country = str.lower(country)
 
 
-    if not (self._number.isdigit()):
-        raise ValueError('Invalid Social Security Number \'{}\''.format(self._number))
+        if not (self._number.isdigit()):
+            raise ValueError('Invalid Social Security Number \'{}\''.format(self._number))
 
-def IsValid(self):
-    """Check if selected Social Security Number is valid """
-    return True
+    @property
+    def isValid(self):
+        """Check if selected Social Security Number is valid """
+        i = 0
+        checknumber = 0
+        control = 0
 
-def Value(self):
-    """Get Social Security Number"""
-    return self._number
+        while i <= 9:
+            if i % 2 == 0:
+                control = int(self._number[i])*2
+            else:
+                control = int(self._number[i])*1
+
+            if control > 9:
+                checknumber = checknumber + int(str(control)[0])+ int(str(control)[1])
+            else:
+                checknumber = checknumber + int(str(control))
+
+            i = i +1
+
+        return checknumber
+
+    def format(self):
+        """Get Social Security Number"""
+        return self._number
+
+    def country(self):
+        """Get selected country"""
+        if self._country == str.lower('sv'):
+            return 'Sweden'
+        elif self._country == str.lower('no'):
+            return 'Norway'
+        elif self._country == str.lower('dk'):
+            return 'Denmark'
+        elif self._country == str.lower('fi'):
+            return 'Finland'
+        else:
+            return 'Unknown option !'
+
+ssn = SSN('dk','19020101-0101')
+print (ssn.isValid)
+print (ssn.country())
 
 # country = input('Enter Country (sv,dk, no, fi): ')
 # ssn = input('Enter Social Security Number (SSN): ')
-# ssn = str.replace(ssn,'-','')
-# ssn = str.replace(ssn,'.','')
-# ssn = ssn.strip()
-#
-# if country == str.lower('sv'):
-#        print ('Sweden')
-# elif country == str.lower('no'):
-#        print ('Norway')
-#else:
-#        print ('Unknown option !')
-#print (ssn)
+
 
